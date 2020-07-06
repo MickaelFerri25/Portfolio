d959a7568632cea622fb0b3e1eb86f10a6a08a9c
MickaelFerri25 <66513258+MickaelFerri25@users.noreply.github.com>
2020-07-06 23:05:32 +0200

    Add files via upload
    Front-end terminé

---
+++ assets/js/barretop.js
@@ -0,0 +1,6 @@
+const i_block = document.querySelector('#i-infos')
+const block_infos = document.querySelector('#block-infos')
+
+i_block.addEventListener('click', () => {
+    block_infos.style.display = "none"
+})
