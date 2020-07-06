e27b6f7801c856c6a8cfd54b581dcfc6660d8a94
MickaelFerri25 <66513258+MickaelFerri25@users.noreply.github.com>
2020-07-06 22:33:56 +0200

    Add files via upload
    Réglages de bugs mineurs

--- assets/js/ScrollReveal.js
+++ assets/js/ScrollReveal.js
@@ -28,3 +28,34 @@ sr.reveal('.contact', {
     duration: 1100,
     reset: false
 });
+
+window.addEventListener('resize', () => {
+    sr.reveal('.about', { 
+        origin: 'left',
+        distance: '50px',
+        duration: 900,
+        reset: false
+    });
+    
+    sr.reveal('.skils', { 
+        origin: 'left',
+        distance: '50px',
+        duration: 900,
+        delay: 900,
+        reset: false
+    });
+    
+    sr.reveal('.projets', { 
+        origin: 'left',
+        distance: '50px',
+        duration: 1100,
+        reset: false
+    });
+    
+    sr.reveal('.contact', { 
+        origin: 'left',
+        distance: '50px',
+        duration: 1100,
+        reset: false
+    });
+})
